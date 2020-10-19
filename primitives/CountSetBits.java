c20119558fec6028f18dc9c09b6d2614b0d1b803
Priyan <63390267+priyan-p@users.noreply.github.com>
2020-10-19 21:58:36 +0530

    To count set bits in an integer

---
+++ primitives/CountSetBits.java
@@ -0,0 +1,27 @@
+public class CountSetBits {
+    
+    public static void main(String[] args) {
+        CountSetBits main = new CountSetBits();
+        System.out.println(main.findNumberOfSetBits1(Integer.MAX_VALUE));
+        System.out.println(main.findNumberOfSetBits2(Integer.MAX_VALUE));
+    }
+    
+    int findNumberOfSetBits2(int n){
+        int setBits = 0;
+        while(n > 0){   // runs only for number of set bits in n
+            setBits++;
+            n &= (n - 1); // clears the right most set bit
+        }
+        return setBits;
+    }
+    
+    int findNumberOfSetBits1(int n){
+        int setBits = 0;
+        while(n > 0){
+            setBits += (n & 1);
+            n >>= 1;
+        }
+        return setBits;
+    }
+    
+}
