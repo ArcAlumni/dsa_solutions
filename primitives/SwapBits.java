6f78d8f96aeba4c7d55121fd157cf1524d711c93
Priyan <63390267+priyan-p@users.noreply.github.com>
2020-10-19 22:44:18 +0530

    Create SwapBits.java

---
+++ primitives/SwapBits.java
@@ -0,0 +1,21 @@
+public class SwapBits {
+    
+    /**
+        Swap the i-th and j-th position bits
+    */
+    
+    public static void main(String[] args) {
+        SwapBits p = new SwapBits();
+        System.out.println(p.swapBits(73, 1, 6));
+    }
+    
+    // flip bits if they are different
+    long swapBits(long x, int i, int j){
+        if(((x >> i) & 1) != ((x >> j) & 1)){ 
+            long mask = (1L << i) | (1L << j); // generate mask with set bits
+            x ^= mask; // XOR of 1 will flip the bit
+        }    
+        return x;
+    }
+    
+}
