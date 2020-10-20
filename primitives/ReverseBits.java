d0061122603202699c742a066e908182d6f4ea9f
Priyan <63390267+priyan-p@users.noreply.github.com>
2020-10-20 14:34:38 +0530

    Create ReverseBits.java

---
+++ primitives/ReverseBits.java
@@ -0,0 +1,26 @@
+public class ReverseBits {
+    
+    /**
+        To reverse bits of an integer
+    */
+    
+    public static void main(String[] args) {
+        ReverseBits r = new ReverseBits();
+        System.out.println(Integer.toBinaryString(687657));
+        System.out.println(Integer.toBinaryString(r.reverseBits(687657)));
+    }
+    
+    // take each LSB from 'x' by >> 
+    // and append to 'rev' by | (OR)
+    // make appended bit as MSB by << 
+    int reverseBits(int x){
+        int rev = 0;
+        for(int i=0;i<32;i++){
+            rev <<= 1;
+            rev |= (x & 1);
+            x >>= 1;
+        }
+        return rev;
+    }
+    
+}
