6259f8d0ab87270dcda7625021b58b59cbcc545f
Priyan <63390267+priyan-p@users.noreply.github.com>
2020-10-20 22:26:53 +0530

    Create ArithmeticUsingBitwise.java

---
+++ primitives/ArithmeticUsingBitwise.java
@@ -0,0 +1,20 @@
+public class ArithmeticUsingBitwise {
+    
+    public static void main(String[] args) {
+        ArithmeticUsingBitwise a = new ArithmeticUsingBitwise();
+        System.out.println(a.add(5, 12));
+    }
+    
+    // build half adder by b = a ^ b
+    // carry using a & b and <<
+    // build full adder by carry ^ b
+    int add(int a, int b){
+        while(a > 0){
+            int carry  = a & b;
+            b ^= a;
+            a = carry << 1;
+        }
+        return b;
+    }
+    
+}
