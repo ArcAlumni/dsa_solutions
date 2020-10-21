7a1062ee74a9ed089baf21a100c8bf218dadbcda
Priyan <63390267+priyan-p@users.noreply.github.com>
2020-10-21 12:22:47 +0530

    Create ReverseInteger.java

---
+++ primitives/ReverseInteger.java
@@ -0,0 +1,18 @@
+public class ReverseInteger {
+    
+    public static void main(String[] args) {
+        ReverseInteger obj = new ReverseInteger();
+        System.out.println(obj.reverse(123456));
+    }
+    
+    int reverse(int x){
+        int result = 0;
+        while(x != 0){
+            result *= 10;
+            result  += x % 10;
+            x /= 10;
+        }
+        return result;
+    }
+    
+}
