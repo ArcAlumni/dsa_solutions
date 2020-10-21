a6c14ecc67a48a95c5d441f4149011e76bd923bd
Priyan <63390267+priyan-p@users.noreply.github.com>
2020-10-21 16:18:22 +0530

    Create ArrangeEvenOdd.java

---
+++ arrays/ArrangeEvenOdd.java
@@ -0,0 +1,28 @@
+public class ArrangeEvenOdd {
+    
+    /**
+        Arrange the array such that all even numbers come first
+    */
+    
+    public static void main(String[] args) {
+        ArrangeEvenOdd obj = new ArrangeEvenOdd();
+        int[] arr = {1,2,3,4,5,6,7};
+        obj.evenOdd(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    void evenOdd(int[] arr){
+        int i = 0, j = arr.length - 1;
+        while(i < j){
+            if(arr[i] % 2 == 0){
+                i++;
+            }else{
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+    
+}
