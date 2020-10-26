da223b291d8f76719cc74e748ccbd7995b35e763
Priyan <63390267+priyan-p@users.noreply.github.com>
2020-10-26 14:23:43 +0530

    Create PascalsTriangle.java

---
+++ arrays/PascalsTriangle.java
@@ -0,0 +1,24 @@
+public class PascalsTriangle {
+
+    public void pascalsTriangle() {
+        
+        List<List<Integer>> list = new ArrayList<>();
+        list.add(Arrays.asList(1));
+        list.add(Arrays.asList(1, 1));
+        
+        for(int i=2;i<10;i++){
+            List<Integer> prev = list.get(i - 1);
+            List<Integer> li = new ArrayList<>();
+            li.add(1);
+            for(int j=0;j<prev.size()-1;j++){
+                li.add(prev.get(j) + prev.get(j + 1));
+            }
+            li.add(1);
+            list.add(li);
+        }
+        
+        System.out.println(list);
+        
+    }
+    
+}
