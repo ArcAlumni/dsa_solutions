0fe1c72acdd4a31ab68345712b18ab0fbb9a3b9e
Priyan <63390267+priyan-p@users.noreply.github.com>
2020-11-06 22:13:28 +0530

    Create ConstructTreeFromInorderPreorder.java

---
+++ tree/ConstructTreeFromInorderPreorder.java
@@ -0,0 +1,33 @@
+class ConstructTreeFromInorderPreorder {
+    
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(preorder, inorder, 0, inorder.length - 1);
+    }
+    
+    int preorderIdx = 0;
+    
+    TreeNode helper(int[] preorder, int[] inorder, int inorderStartIdx, int inorderEndIdx){
+    
+        if(preorderIdx >= preorder.length || inorderStartIdx > inorderEndIdx)
+            return null;
+        
+        int rootValue = preorder[preorderIdx++];
+        
+        TreeNode root = new TreeNode(rootValue);
+
+        if(inorderEndIdx == inorderStartIdx)
+            return root;
+        
+        int i;
+        for(i=inorderStartIdx;i<=inorderEndIdx;i++){
+            if(inorder[i] == rootValue)
+                break;
+        }
+        
+        root.left = helper(preorder, inorder, inorderStartIdx, i - 1);
+        root.right = helper(preorder, inorder, i + 1, inorderEndIdx);
+        
+        return root;
+    }
+    
+}
